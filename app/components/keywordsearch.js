014d5d7a6c2f7c20433fc8821bc78727cd597ba2
HarikumarG <harikumarmgp@gmail.com>
2020-07-17 22:56:02 +0530

    Keysearch full complete

--- app/components/keywordsearch.js
+++ app/components/keywordsearch.js
@@ -7,17 +7,29 @@ export default class KeywordsearchComponent extends Component {
 
     @tracked loginpage = true;
     @tracked searchpage = false;
+    @tracked analytics = false;
+    @tracked showAnalytics = false;
+    @tracked showChart = false;
+    @tracked analyticsdata = undefined;
+    @tracked chartType = false;
 
     folderpath = "";
     filename = "";
     lineNo = 1;
+    keyword = undefined;
+    responsedata = undefined;
+    folderregexlinux = /^\/$|(\/[a-zA-Z_0-9-]+)+\//;
 
     @action onValidate() {
-        if(this.folderInput != undefined && this.fileInput != undefined) {
-            this.checkpathReq();            
-        } else {
+        if(this.folderInput == undefined || this.fileInput == undefined) {
             alert("Fill all the fields");
             location.reload();
+        } else if(this.folderInput != undefined && !this.folderregexlinux.test(this.folderInput)) {
+            alert("Enter valid Folder Path");
+            location.reload();
+        }
+        else {
+            this.checkpathReq();
         }
     }
 
@@ -28,6 +40,25 @@ export default class KeywordsearchComponent extends Component {
             alert("Enter the keyword to be searched");
         }
     }
+
+    @action onAnalytics() {
+        if(this.keyword != undefined) {
+            this.getAnalyticsReq();
+            this.chartType = true;
+        } else {
+            alert("Keyword is undefined");
+        }
+    }
+
+    @action onChartView() {
+        this.charts(this.responsedata);
+    }
+
+    @action onNormalView() {
+        this.showAnalytics = true;
+        this.showChart = false;
+    }
+
     checkpathReq() {
         jQuery.ajax({
             url:"http://localhost:8080/Searching-Backend/checkpath",
@@ -64,14 +95,32 @@ export default class KeywordsearchComponent extends Component {
                 "keyword":this.keywordInput
             })
         }).then((response) => {
-            var keyword = this.keywordInput;
-            for(var key in response[keyword]) {
-                var sentence = response[keyword][key];
+            this.keyword = this.keywordInput;
+            for(var key in response[this.keyword]) {
+                var sentence = response[this.keyword][key];
                 if(sentence.length > 0) {
-                    this.displaytoDOM(sentence,keyword,this.lineNo);
+                    this.displaytoDOM(sentence,this.keyword,this.lineNo);
                     this.lineNo++;
                 }
             }
+            this.analytics = true;
+        }).catch(function(error) {
+            console.log(error);
+        })
+    }
+    getAnalyticsReq() {
+        jQuery.ajax({
+            url:"http://localhost:8080/Searching-Backend/analytics",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType:"json",
+            data: JSON.stringify({
+                "keyword":this.keyword
+            })
+        }).then((response) => {
+            console.log(response);
+            this.responsedata = response;
+            this.charts(response);
         }).catch(function(error) {
             console.log(error);
         })
@@ -99,5 +148,56 @@ export default class KeywordsearchComponent extends Component {
         trow.appendChild(tdata2);
 
         tbody.appendChild(trow);
-    } 
+    }
+
+    async charts(data) {
+        this.showChart = true;
+        this.showAnalytics = false;
+        await google.charts.load('current', {packages: ['corechart']});
+        var searches = data["searchcount"];
+        var results = data["resultcount"];
+        var time = data["timetaken"];
+        let d = {
+            smaxkey:searches[0]["keyname"],
+            smaxval:searches[0]["keyval"],
+            sckey:searches[1]["keyname"],
+            scval:searches[1]["keyval"],
+            sminkey:searches[2]["keyname"],
+            sminval:searches[2]["keyval"],
+            rmaxkey:results[0]["keyname"],
+            rmaxval:results[0]["keyval"],
+            rckey:results[1]["keyname"],
+            rcval:results[1]["keyval"],
+            rminkey:results[2]["keyname"],
+            rminval:results[2]["keyval"],
+            tmaxkey:time[0]["keyname"],
+            tmaxval:time[0]["keyval"],
+            tckey:time[1]["keyname"],
+            tcval:time[1]["keyval"],
+            tminkey:time[2]["keyname"],
+            tminval:time[2]["keyval"],
+        }
+        this.analyticsdata = d;
+        this.drawChart(d);
+    }
+    drawChart(data) {
+        var d = google.visualization.arrayToDataTable([
+            ['Type',      'MAX',{ role: 'annotation'}, 'MIN',{ role: 'annotation'}, 'CURRENT',{ role: 'annotation'}],
+            ['Searches',  data.smaxval,data.smaxkey,   data.sminval,data.sminkey,    data.scval,data.sckey],
+            ['Results',   data.rmaxval,data.rmaxkey,   data.rminval,data.rminkey,    data.rcval,data.rckey]
+        ]);
+
+        var options = {title: 'Searches (in number of times searched)\nResults (in number of lines obtained)'};
+        var chart = new google.visualization.BarChart(document.getElementById('data_div'));
+        chart.draw(d, options);
+
+        var timedata = google.visualization.arrayToDataTable([
+            ['Type',      'MAX',{ role: 'annotation'}, 'MIN',{ role: 'annotation'}, 'CURRENT',{ role: 'annotation'}],
+            ['Time Taken', data.tmaxval,data.tmaxkey,    data.tminval,data.tminkey, data.tcval,data.tckey]
+        ]);
+
+        var timeoptions = {title: 'Time (in Nano seconds)'};
+        var timechart = new google.visualization.BarChart(document.getElementById('time_div'));
+        timechart.draw(timedata,timeoptions);
+    }
 }
